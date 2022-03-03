ce6ebb18ef4174c5925e45e3349a0b72949a38ea
Malte Stausholm Boelskifte <au666081@uni.au.dk>
2022-03-03 14:01:04 +0100

    Add project files.

---
+++ Ladeskab1/IDoor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ladeskab1
+{
+    interface IDoor
+    {
+
+        void Lock();
+        void Unlock();
+    }
+}
