ec718688100c88bbe93f0cb8441de67bc28df15e
Kaare Dalgaard Knudsen <au669950@uni.au.dk>
2022-03-23 19:00:31 +0100

    Test, ID not correct, Doorlock, DoorOpen

--- Ladeskab1/StationControl/StationControl.cs
+++ Ladeskab1/StationControl/StationControl.cs
@@ -20,7 +20,7 @@ namespace Ladeskab1
         // Her mangler flere member variable
         private LadeskabState _state;
         private IChargeControl _charger;
-        private int _oldId;
+        public int _oldId;
         private IDoor _door;
         private IDisplay _display;
         private IReader _reader;
